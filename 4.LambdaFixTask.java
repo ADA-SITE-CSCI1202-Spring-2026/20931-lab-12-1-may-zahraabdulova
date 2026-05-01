e5263ee08bd57fe9211d0ab60a6d57aceec5e00d
zahraabdulova <zabdulova16110@ada.edu.az>
2026-05-01 09:41:02 +0400

    Create 4.LambdaFixTask.java

---
+++ 4.LambdaFixTask.java
@@ -0,0 +1,17 @@
+@FunctionalInterface
+interface NumberChecker {
+    boolean check(int number);
+
+    default void printResult() {
+        System.out.println("Checking...");
+    }
+}
+
+public class LambdaFixTask {
+    public static void main(String[] args) {
+
+        NumberChecker isEven = number -> number % 2 == 0;
+
+        System.out.println(isEven.check(10));
+    }
+}
